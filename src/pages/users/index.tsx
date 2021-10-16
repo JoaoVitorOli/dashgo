bbbf359eb97a582d82dd6c5df4fc5c460056b40d
João Vitor <infamoz_br3@hotmail.com>
2021-10-16 12:54:14 -0300

    :shipit: pagina listagem de usuarios feita

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Box, Button, Flex, Text, Heading, Icon, Table, Thead, Tbody, Tr, Th, Checkbox, Td } from "@chakra-ui/react";
+
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+import { RiAddLine } from "react-icons/ri";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">João Vitor</Text>
+                    <Text fontSize="sm" color="gray.300">joao@email.com</Text>
+                  </Box>
+                </Td>
+                <Td>04 de abril, 2021</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
