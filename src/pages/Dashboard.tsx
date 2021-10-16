f8cefe7000ea4808e3c6337828260301a28bf6d7
João Vitor <infamoz_br3@hotmail.com>
2021-10-16 11:09:57 -0300

    :shipit: componente sidebar feito

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,22 @@
-export function Dashboard() {
+import { Flex } from "@chakra-ui/react";
+
+import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
+
+export default function Dashboard() {
   return (
-    <h1>Dashboard</h1>
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex
+        w="100%"
+        my="6"
+        maxWidth={1480}
+        mx="auto"
+        px="6"
+      >
+        <Sidebar />
+      </Flex>
+    </Flex>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,10 @@ import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import { theme } from "../styles/theme";
-import { Header } from "../components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Header />
       <Component {...pageProps} />
     </ChakraProvider>
   )
