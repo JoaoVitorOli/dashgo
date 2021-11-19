2abdd098b9305b1bf373f45658176676c853ca3f
João Vitor <infamoz_br3@hotmail.com>
2021-11-19 19:54:03 -0300

    lista usuarios e configura ReactQueryDevTools

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -29,7 +29,20 @@ export default function UserList() {
     const response = await fetch("https://localhost:3000/api/users");
     const data = await response.json();
 
-    return data;
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: "2-digit",
+          month: "long",
+          year: "numeric"
+        }),
+      }
+    });
+
+    return users;
   });
 
   const isWideVersion = useBreakpointValue({
@@ -82,18 +95,22 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">João Vitor</Text>
-                        <Text fontSize="sm" color="gray.300">joao@email.com</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 de abril, 2021</Td>}
-                  </Tr>
+                  {data.map(user => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={["4", "4", "6"]}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                      </Tr>
+                    )
+                  })}
                 </Tbody>
               </Table>
 
